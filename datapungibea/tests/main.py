c17d710c1e9aadf17a4b9aa0494136e564bdafed
James Otterson <jjotterson@gmail.com>
2019-07-16 22:34:30 -0400

    testRun reading folder location from userSettings

--- datapungibea/tests/main.py
+++ datapungibea/tests/main.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-#from ..utils import getResourcePath
+from datapungibea.utils import getUserSettings
 
 
 def runTests(outputPath='',testsPath='',verbose = True):
@@ -8,7 +8,11 @@ def runTests(outputPath='',testsPath='',verbose = True):
        testsPath =  os.path.dirname(os.path.abspath(__file__)).replace("\\","/")
        print('**************************** \nWill run tests in: ' + testsPath)
     if not outputPath:
-        outputPath = "C:/"
+        try:
+            settingsFile = getUserSettings()
+            outputPath = settingsFile['TestsOutputPath']
+        except:
+            print("Could not load TestOutputPath from user settings.  Perhaps run util.setTestFolder( FilePath )  ")
     subprocess.Popen('pytest ' + testsPath + ' --html='+outputPath+'datapungibea_Tests.html --self-contained-html')
     if verbose:
         print('Tests will be saved in '+outputPath+'datapungibea_Tests.html \n****************************')
