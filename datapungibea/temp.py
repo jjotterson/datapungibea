b0a15d228e0539c05aceeaec3533275e5ddcbc21
James Otterson <jjotterson@gmail.com>
2019-07-29 02:43:17 -0400

    next steps indentation: find missing cases

--- datapungibea/temp.py
+++ datapungibea/temp.py
@@ -33,7 +33,7 @@ def toDictionary(indentArrayTuples):
   for entry in indentArrayTuples:
     output.append({'tableNames':list(set(entry[0])), 'structure':pd.DataFrame(list(zip( list(entry[1]), list(entry[2]) )),columns = ['SeriesCode','Indentation']).to_dict('records')}  )
   return(output)
-
+#TODO: change this format: dictionary of 3 arrays: tableNames, SeriesCode and Indentations.  dump as json
   
 data = dpb.data()
 
@@ -44,3 +44,23 @@ for section in range(1,8):
     all = getIndentations(v,all)
 
 cleanAll = toDictionary(all)
+
+
+#TODO:
+# 1 check no table shows up as two different keys - else use try to keep trying to find the right format on the given 
+#2 check all current tables have indentation info  (first get nipa parameter of tables with getParameterValues, check if the tables are somewhere in the list)
+
+
+flat_list = []
+for sublist in all:
+    sublist = list(sublist[0])
+    for item in sublist:
+        flat_list.append(item)
+
+flat_list = list(set(flat_list))
+
+pv = data.getParameterValues('NIPA','TableName')
+pv = list(pv['TableName'])
+
+
+set(pv).issubset(flat_list)
