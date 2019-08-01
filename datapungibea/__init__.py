be4510dc09380a4efd56a399138f38ec63287d90
James Otterson <jjotterson@gmail.com>
2019-08-01 01:10:58 -0400

    packing for publishing

--- datapungibea/__init__.py
+++ datapungibea/__init__.py
@@ -1,6 +1,11 @@
-#__all__ = ['api']
+"""Connects to BEA API and enriches data output; get vintage BEA data"""
+import pandas
+import requests
 
 from datapungibea.api import *
 import datapungibea.tests as tests
+
+__version__ = '0.8'
+#__all__ = ['api']
 
 
