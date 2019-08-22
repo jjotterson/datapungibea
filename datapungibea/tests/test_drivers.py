973c9243018c0a7f3171f0afba0a6214a637dafe
James Otterson <JamesOt@cbo.gov>
2019-08-22 11:14:39 -0400

    tests run separatedly and yaml code snippet pass

--- datapungibea/tests/test_drivers.py
+++ datapungibea/tests/test_drivers.py
@@ -18,27 +18,25 @@ def executeCode(stringIn):
         except:
             return(dict(codeRun = False, codeOutput = pd.DataFrame([])))
 
-# content of test_sample.py
-def test_startDriver(cmdopt):
-    global dataBea
+# start  the driver - used by all tests
+def startDriver(cmdopt):
     if not cmdopt == "":
         connectionParameters = {"key": cmdopt, "url": "https://apps.bea.gov/api/data/"}
     else:
         connectionParameters = {}
     dataBea = dpbea.data(connectionParameters)
+    return(dataBea)    
+
+# content of test_sample.py
+def test_startDriver(cmdopt):
+    dataBea = startDriver(cmdopt)
     assert dataBea
-    return(dataBea)
 
 def test_datasetlist(cmdopt):
     '''
       test the datasetlist BEA dataset
     '''
-    global dataBea
-    if not cmdopt == "":
-        connectionParameters = {"key": cmdopt, "url": "https://apps.bea.gov/api/data/"}
-    else:
-        connectionParameters = {}
-    dataBea = dpbea.data(connectionParameters)
+    dataBea = dataBea = startDriver(cmdopt)
     driver   = dataBea.datasetlist(verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200               #test if connection was stablished
@@ -47,7 +45,8 @@ def test_datasetlist(cmdopt):
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
     #print('datasetlist driver is working (Request OK, data cleaned, can run code snippet, snippet agrees with request)!')
 
-def test_getParameterList():
+def test_getParameterList(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.getParameterList('FixedAssets',verbose=True)  
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -55,7 +54,8 @@ def test_getParameterList():
     assert execCode['codeRun']                   #try to execute the code.
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_getParameterValues():
+def test_getParameterValues(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.getParameterValues('NIPA','Year',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -63,15 +63,17 @@ def test_getParameterValues():
     assert execCode['codeRun']                   #try to execute the code.
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_NIPA():
-    driver = dataBea.NIPA('T10101','Q','2010',verbose=True)
+def test_NIPA(cmdopt):
+    dataBea = startDriver(cmdopt)
+    driver = dataBea.NIPA('T10101','Q','2010',verbose=True,includeIndentations=False)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
     assert not driver['dataFrame'].empty         #cleaned up output is not empty
     assert execCode['codeRun']                   #try to execute the code.
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_fixedAssets():
+def test_fixedAssets(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.fixedAssets('FAAt101','2013',verbose=True)   #try all years 
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -79,7 +81,8 @@ def test_fixedAssets():
     assert execCode['codeRun']           #try to execute the code.   
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_ITA():
+def test_ITA(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.ITA('BalCurrAcct','Brazil','A','2010',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -87,7 +90,8 @@ def test_ITA():
     assert execCode['codeRun']            #try to execute the code.   
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the      
 
-def test_IIP():
+def test_IIP(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.IIP(TypeOfInvestment='DebtSecAssets',Component='All',Frequency='All',Year='2010',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -95,7 +99,8 @@ def test_IIP():
     assert execCode['codeRun']        #try to execute the code.       
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the      
 
-def test_GDPbyIndustry():
+def test_GDPbyIndustry(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.GDPbyIndustry('211','1','A','2018',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -103,7 +108,8 @@ def test_GDPbyIndustry():
     assert execCode['codeRun']          #try to execute the code.   
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the          
 
-def test_InputOutput():
+def test_InputOutput(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.InputOutput(TableID='56',Year='2010',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -111,7 +117,8 @@ def test_InputOutput():
     assert execCode['codeRun']            #try to execute the code.       
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_UnderlyingGDPbyIndustry():
+def test_UnderlyingGDPbyIndustry(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.UnderlyingGDPbyIndustry('ALL','ALL','A','2014',verbose=True)   #try all years and check line 40788
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -119,7 +126,8 @@ def test_UnderlyingGDPbyIndustry():
     assert execCode['codeRun']           #try to execute the code.       
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_IntlServTrade():
+def test_IntlServTrade(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.IntlServTrade('ALL','ALL','ALL','AllCountries','All',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -127,14 +135,16 @@ def test_IntlServTrade():
     assert execCode['codeRun']          #try to execute the code.       
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the  
 
-def test_IntlServTrade():
+def test_IntlServTrade(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.IntlServTrade('ALL','ALL','ALL','AllCountries','All',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
     assert not driver['dataFrame'].empty         #cleaned up output is not empty
     assert execCode['codeRun']         #try to execute the code.       
 
-def test_Regional():
+def test_Regional(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.Regional('00000','1','SAGDP5N', 'All',verbose=True)
     execCode = executeCode(driver['code']) 
     assert driver['request'].status_code == 200  #test if connection was stablished
@@ -142,7 +152,8 @@ def test_Regional():
     assert execCode['codeRun']           #try to execute the code.       
     assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the      
 
-def test_NIPASummary():
+def test_NIPASummary(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.NIPASummary('2012','Q',verbose=True)
     #execCode = executeCode(driver['code']) 
     #assert driver['request'].status_code == 200  #test if connection was stablished
@@ -150,7 +161,8 @@ def test_NIPASummary():
     #assert execCode['codeRun']           #try to execute the code.       
     #assert execCode['codeOutput'].equals(driver['dataFrame']) #test if the output of the code equals the output of the      
 
-def test_NIPAVintage():
+def test_NIPAVintage(cmdopt):
+    dataBea = startDriver(cmdopt)
     driver = dataBea.NIPAVintage(tableName = 'T10101', Title = 'Section 1',year = '2018', quarter ='Q1',vintage='Second',verbose = True)
     execCode = executeCode(driver['code']) 
     #assert driver['request'].status_code == 200  #test if connection was stablished
