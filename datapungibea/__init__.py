c04c2498a7fb98e4af098b573b1c6eb826dec23e
James Otterson <jjotterson@gmail.com>
2019-08-23 01:02:31 -0400

    tagging v1

--- datapungibea/__init__.py
+++ datapungibea/__init__.py
@@ -1,11 +1,11 @@
-"""Connects to Bureau of Economic Analysis (BEA) API and enriches data output; get vintage BEA data"""
+"""Connects to Bureau of Economic Analysis (BEA) API and enriches data output; get vintage data"""
 import pandas
 import requests
 
 from datapungibea.api import *
 import datapungibea.tests as tests
 
-__version__ = '0.9.5'
+__version__ = '1.0.0'
 #__all__ = ['api']
 
 
